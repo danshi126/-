8078a90d2fd77f08bb35dd3130602237c3dab269
apple <apple@appledeMacBook-Pro.local>
2015-05-04 21:18:31 +0800

    放大

--- 超级猜图/ViewController.m
+++ 超级猜图/ViewController.m
@@ -9,19 +9,61 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *iconButton;
+
+@property (strong,nonatomic) UIButton *conver;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+- (UIButton *)conver
+{
+    if(_conver == nil)
+    {
+        _conver = [[UIButton alloc] initWithFrame:self.view.bounds];
+        _conver.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+        [self.view addSubview:_conver];
+        _conver.alpha = 0.0;
+        [_conver addTarget:self action:@selector(smallImage:) forControlEvents:(UIControlEventTouchUpInside)];
+        
+    }
+    return _conver;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)BigImage:(UIButton *)but
+{
+    [self conver];
+    
+    CGFloat w = self.view.bounds.size.width;
+    CGFloat h = self.view.bounds.size.height - w;
+    h = h * 0.5;
+    
+    [self.view bringSubviewToFront:self.iconButton];
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        self.iconButton.frame = CGRectMake(0, h, w, w);
+        self.conver.alpha = 1.0;
+    }];
+    
+    
+    
+}
+
+
+- (void)smallImage:(UIButton *)but
+{
+    [UIView animateWithDuration:1.0f animations:^{
+        self.iconButton.frame = CGRectMake(100, 100, 120, 120);
+        but.alpha = 0.0;
+    }];
+}
+
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
 }
 
 @end
